4540a43261c79dbf56412693c6f5e7c67ec78fc7
Artur Teixeira <1110119@isep.ipp.pt>
2013-12-12 17:18:56 +0000

    Library of standard graphs ref #1, #2, #3

---
+++ Graphs4Social/graphs.h
@@ -0,0 +1,37 @@
+#ifndef _GRAPH_INCLUDE
+#define _GRAPH_INCLUDE
+
+#define _MAX_NODES_GRAPH 100
+#define _MAX_PATHS_GRAPH 200
+
+#define INFINITESIMAL 0.0001
+
+typedef struct Node{
+	float x, y, z, width;
+}Node;
+
+typedef struct Path{
+	int nodei, nodef;
+	float weight, width;
+}Path;
+
+extern Node nodes[];
+extern Path paths[];
+extern int numNodes, numPaths;
+
+void addNode(Node);
+void deleteNode(int);
+void printNode(Node);
+void listNodes();
+Node createNode(float, float, float);
+
+void addPath(Path);
+void deletePath(int);
+void printPath(Path);
+void listPaths();
+Path createPath(int, int, float, float);
+
+void saveGraph();
+void readGraph();
+
+#endif
