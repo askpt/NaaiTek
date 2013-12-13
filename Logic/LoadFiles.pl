54f9cd9bf9094eb195651cb134a97659141e9032
jpcarreira <jpmavcarreira@gmail.com>
2013-12-13 16:13:31 +0000

    added file loader for all Prolog logv

---
+++ Logic/LoadFiles.pl
@@ -0,0 +1,12 @@
+% 
+% Loads all files necessary for Logic module
+%
+
+% loads knowledge base
+['KnowledgeBase.pl'].
+
+% loads basic basic predicates
+['BasicPredicates.pl'].
+
+% loads graph search algorittms
+['GraphSearchAlgorithms.pl'].
