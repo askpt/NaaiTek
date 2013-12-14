e56e3914fa939d4f5275b20c40323753b07ffd0e
jpcarreira <jpmavcarreira@gmail.com>
2013-12-14 18:09:16 +0000

    added branch and bound algorithm ref #6

--- Logic/GraphSearchAlgorithms.pl
+++ Logic/GraphSearchAlgorithms.pl
@@ -20,7 +20,7 @@ depth_search(PersonA, PersonB, TempList, [PersonA|List]) :- isConnected(PersonA,
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % breadth search
-% this predicates gets a possible path between PersonA and PersonB,
+% this predicate gets a possible path between PersonA and PersonB,
 % taking into account the less possible number of nodes needed to go through
 %
 % note: it is a breadth search, it doesn't guarantee that this is better
@@ -40,4 +40,26 @@ breadth_search_aux([[PersonB | Tail] | Remaining], PersonB, List) :- !, breadth_
 next_node(X, T, Z) :- connects(X, Z, _), 
 						not(member(Z, T)).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% branch and bound search
+% this search method evaluates local transistions but always evaluates, in any given moment, whether
+% it would be better, or not, to change which node to expand; this evaluation is based on the accumulated
+% strength from the root node until the evaluated node (this method will allow to evaluate the path, in the 
+% social graph, that guarantees that is going through the "strongest" relationships)
+
+branch_and_bound(PersonA, PersonB, List) :- branch_and_bound_aux([(0, [PersonA])], PersonB, Path), reverse(Path, List).
+
+branch_and_bound_aux([(_, First) | _], PersonB, First) :- First = [PersonB | _].
+
+branch_and_bound_aux([(_, [PersonB | _]) | Remaining], PersonB, Path) :- !, branch_and_bound_aux(Remaining, PersonB, Path).
+
+branch_and_bound_aux([(Cost, [Last | Tail]) | Others], PersonB, Path) :- 
+					findall((CostC, [PersonZ, Last | Tail]),
+					(next_node_b_b(Last, Tail, PersonZ, CostC1), CostC is CostC1 + Cost), List),
+					append(Others, List, PathN),
+					sort(PathN, PathN1),
+					branch_and_bound_aux(PathN1, PersonB, Path).
+
+next_node_b_b(PersonX, List, PersonZ, Cost) :- connects(PersonX, PersonZ, Cost), not(member(PersonZ, List)).
 
