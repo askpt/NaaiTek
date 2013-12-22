d3f66b24d62937d2c3dc9196026ba9271f2ce31f
Artur Teixeira <1110119@isep.ipp.pt>
2013-12-22 19:11:40 +0000

    100th commit! Translations (.resx files) are now available on all pages! ref #16

--- WebSocial/Controllers/HomeController.cs
+++ WebSocial/Controllers/HomeController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebSocial.Helpers;
 
 namespace WebSocial.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
@@ -25,6 +26,24 @@ namespace WebSocial.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
         }
     }
 }
--- WebSocial/Controllers/TagController.cs
+++ WebSocial/Controllers/TagController.cs
@@ -155,22 +155,5 @@ namespace WebSocial.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult SetCulture(string culture)
-        {
-            // Validate input
-            culture = CultureHelper.GetImplementedCulture(culture);
-            // Save culture in a cookie
-            HttpCookie cookie = Request.Cookies["_culture"];
-            if (cookie != null)
-                cookie.Value = culture;   // update cookie value
-            else
-            {
-                cookie = new HttpCookie("_culture");
-                cookie.Value = culture;
-                cookie.Expires = DateTime.Now.AddYears(1);
-            }
-            Response.Cookies.Add(cookie);
-            return RedirectToAction("Index");
-        }
     }
 }
