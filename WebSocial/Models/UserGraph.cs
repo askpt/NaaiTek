322f96886ff1e3533b4195c6eea4763d86dffb5d
André Silva <andres_silva@outlook.com>
2013-12-28 16:01:07 +0000

    Added the model of the Json Graph ref #31, #34

---
+++ WebSocial/Models/UserGraph.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSocial.Models
+{
+    public class Path
+    {
+        public string user1 { get; set; }
+        public string user2 { get; set; }
+        public int connection { get; set; }
+        public List<string> tags { get; set; }
+    }
+
+    public class UserGraph
+    {
+        public List<string> nodes { get; set; }
+        public List<Path> paths { get; set; }
+        public string status { get; set; }
+    }
+}
