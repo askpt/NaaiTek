193764e80d6c7ed9b433b1f08a9462fd5fc88f91
André Silva <andres_silva@outlook.com>
2013-12-28 16:20:40 +0000

    Created the service that will receive Json Objects ref #31, 34

---
+++ WebSocial/Helpers/UserGraphServices.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using WebSocial.Models;
+
+namespace WebSocial.Helpers
+{
+    public class UserGraphServices
+    {
+        private const string _baseUrl = "http://localhost:5000";
+
+        public static async Task<UserGraph> GetUserGraph(string user)
+        {
+            UserGraph userGraph;
+
+            HttpClient client = new HttpClient();
+
+            var url = string.Format(_baseUrl + "/get_graph?user={0}", user);
+
+            string responseJson = await client.GetStringAsync(url);
+
+            userGraph = JsonConvert.DeserializeObject<UserGraph>(responseJson);
+
+            return userGraph;
+        }
+    }
+}
