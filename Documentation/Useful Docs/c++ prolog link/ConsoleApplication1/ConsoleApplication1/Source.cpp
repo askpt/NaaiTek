2146e36fe93a9fb9c1bb1c4ff8b3424796c181d6
Artur Teixeira <1110119@isep.ipp.pt>
2013-12-29 17:24:24 +0000

    Research about the subject: Started with a template for tic-tac-toe. Credits to: http://javafree.uol.com.br/topic-852645-Prolog-jogo-da-velha.html Took a closer look at the connection between C++ and PROLOG. ref #askpt

--- Documentation/Useful Docs/c++ prolog link/ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ Documentation/Useful Docs/c++ prolog link/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -5,9 +5,9 @@ int main(){
 	char* argv[] = { "swipl.dll", "-s", "teste.pl", NULL };      
 	PlEngine e(3, argv);
 	PlTermv av(2);
-	av[1] = PlCompound("isaque");
-	PlQuery q("ancestral", av);    
-	while (q.next_solution())  
+	av[1] = PlCompound("isaque"); //2 argumento de ancestral(1,2).
+	PlQuery q("ancestral", av);     // regra a ser chamada
+	while (q.next_solution())  //obter todas as soluções
 	{ 
 		cout << (char*)av[0] << endl; 
 	}    
