e25f3e5b1cbed456caa6ab46fd70c9dfa1113f63
André Silva <andres_silva@outlook.com>
2014-01-03 12:00:16 +0000

    Added a method to get all third friends with same tag. If not exists, the list will be replaced with all third friends. ref #35

--- WebSocial/Controllers/FriendController.cs
+++ WebSocial/Controllers/FriendController.cs
@@ -32,12 +32,69 @@ namespace WebSocial.Controllers
 
                 ViewBag.friends = friends;
                 ViewBag.username = user.UserName;
+
+                ViewBag.suggestedFriends = await GetSuggestedFriends(user.UserName, userID);
+
             }
 
             return View();
         }
 
-        
+        private async Task<List<string>> GetSuggestedFriends(string username, string userId)
+        {
+            List<string> suggestedFriends = new List<string>();
+            IList<int> userTagIDs = (from userToTag in db.UsersTags where (userToTag.UserID == userId) select userToTag.TagID).ToList();
+
+            suggestedFriends = await GetFriendsSuggestedThirdLevel(username, userTagIDs);
+
+            return suggestedFriends;
+        }
+
+        private async Task<List<string>> GetFriendsSuggestedThirdLevel(string username, IList<int> userTagIDs)
+        {
+            List<string> suggestedFriends = new List<string>();
+
+            List<string> thirdLevelFriends = await Services.GetThirdLevelFriends(username);
+
+            if (thirdLevelFriends.Count > 0)
+            {
+                List<string> thirdLevelFriendsId = FindFriendIds(thirdLevelFriends);
+
+                foreach (string friendId in thirdLevelFriendsId)
+                {
+                    IList<int> friendTagIds = (from userToTag in db.UsersTags where (userToTag.UserID == friendId) select userToTag.TagID).ToList();
+
+                    if (userTagIDs.Intersect(friendTagIds).ToList().Count > 0)
+                    {
+                        ApplicationUser friend = db.Users.Find(friendId);
+
+                        suggestedFriends.Add(friend.UserName);
+                    }
+                }
+
+                if (suggestedFriends.Count == 0)
+                {
+                    suggestedFriends = thirdLevelFriends;
+                }
+            }
+
+            return suggestedFriends;
+        }
+
+        private List<string> FindFriendIds(List<string> usernames)
+        {
+            List<string> userIds = new List<string>();
+
+            foreach (ApplicationUser item in db.Users)
+            {
+                if (usernames.Contains(item.UserName))
+                {
+                    userIds.Add(item.Id);
+                }
+            }
+
+            return userIds;
+        }
 
         //
         // GET: /Friend/Details/5
