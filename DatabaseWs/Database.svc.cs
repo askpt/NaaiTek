ec9acbbc776f911c61311c49830fa8c19c3e9d5a
André Silva <andres_silva@outlook.com>
2014-01-07 17:05:04 +0000

    Added a WCF Restfull Webservice to deal with database information. ref #24

---
+++ DatabaseWs/Database.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DatabaseWs
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Database" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Database.svc or Database.svc.cs at the Solution Explorer and start debugging.
+    public class Database : IDatabase
+    {
+        public string DoWork()
+        {
+            return "Test";
+        }
+    }
+}
