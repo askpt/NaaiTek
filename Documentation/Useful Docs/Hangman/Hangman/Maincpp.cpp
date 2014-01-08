a540f8943e5f190b190cea93557cd19d0d618ade
1110333TiagoPacheco <tiago_leal25@hotmail.com>
2014-01-08 11:35:28 +0000

    Added more draw basic functions, for each numer of characters of the word the method draw that numer of lines ref #19

--- Documentation/Useful Docs/Hangman/Hangman/Maincpp.cpp
+++ Documentation/Useful Docs/Hangman/Hangman/Maincpp.cpp
@@ -14,11 +14,25 @@
 
 using namespace std;
 
+/*struct with game usefull data*/
+typedef struct
+{
+	char wrongChar[8];
+	char writeChar[25];
+	bool endGame;
+	bool userWin;
+	int numErrors;
+}Game;
+
 /*global variables*/
 int WindowWidth = 800;
 int WindowHeight = 600;
 const float DEG2RAD = 314159 / 180;
 int GLUTWindowHandle = 0;
+Game game;
+
+/*instructions to hide console when the project are executing*/
+//#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 
 /*initial declaration of some importante functions*/
 void drawScene();
@@ -29,7 +43,27 @@ void keyboard(int key, int x, int y);
 void drawCircle(float centerX, float centerY, float radius, int num, GLfloat lineWidth);
 void drawLine(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2, GLfloat lineWidth);
 void drawChar(int x, int y, char c);
+void initGameData();
+void drawErrorChar();
+void drawWord(string word);
 
+
+/*function to init game data*/
+
+void initGameData()
+{
+	for (int i = 0; i < 8; i++)
+	{
+		game.wrongChar[i] = NULL;
+	}
+	for (int i = 0; i < 25; i++)
+	{
+		game.writeChar[i] = NULL;
+	}
+	game.endGame = false;
+	game.userWin = false;
+	game.numErrors = 0;
+}
 
 /*function when the window is resixed*/
 void handleResize(int x, int y)
@@ -92,17 +126,62 @@ void drawChar(int x, int y, char c)
 	glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
 }
 
+/*function to draw char that does not belong in the word*/
+void drawErrorChar()
+{
+	int cont = 0;
+	for (int i = 0; i < 8; i++)
+	{
+		if (game.wrongChar[i] != NULL)
+		{
+			cont++;
+		}
+	}
+	for (int i = 0; i < cont; i++)
+	{
+		if (i < 4)
+		{
+			if (i == 0)
+			{
+				drawChar(100, 100, game.wrongChar[i]);
+			}
+			else
+				drawChar(100 + (50 * i), 100, game.wrongChar[i]);
+		}
+		else
+		for (int j = 0; j < 4;j++){
+			drawChar(100 + (50 * j), 130, game.wrongChar[i]);
+		}
+	}
+
+}
+/*function to draw char lines of a word*/
+void drawWord(string word)
+{
+	int wordSize = word.length();
+	for (int i = 0; i < wordSize; i++)
+	{
+		drawLine(20+(i*60), 550, 60+(i*60), 550, 3.0);
+	}
+}
 /*function to draw the main scene*/
 void drawScene()
 {
 	glColor3b(1.0, 0.0, 0.0);
-	drawCircle(200, 200, 100, 1000,6.0);
-	drawLine(400, 500, 450, 400,6.0);
-	drawChar(100, 100,'B');
+	//drawLine(400, 500, 450, 400,6.0);
+	for (int i = 0; i<8; i++)
+	{
+		game.wrongChar[i] = 'A';
+	}
+	drawErrorChar();
+	drawWord("arroz");
 }
+
+
 
 int main(int argc, char**argv)
 {
+	initGameData();
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(WindowWidth, WindowHeight);
